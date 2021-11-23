80d15afc8e665f4f15a896281c2259e39660a513
Artem Hermaniuk <ahermaniuk@netfully.com>
2021-11-23 16:51:09 +0200

    Scketch of first logic

---
+++ Domain/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UndergroundProject.Domain.Enteties;
+
+namespace UndergroundProject.Domain
+{
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                Name = "admin",
+                NormalizedName = "ADMIN"
+            });
+        }
+            
+
+        public DbSet<Post> Post { get; set; }
+    }
+}
---
+++ Domain/Enteties/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UndergroundProject.Domain.Enteties
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
